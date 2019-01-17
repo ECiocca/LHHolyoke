64f7d0a4fc00fdb3464ee76fac289ca1f8cf59b9
Eric <ericciocca@gmail.com>
2019-01-17 10:19:46 -0500

    Auto stash before merge of "master" and "origin/master"

---
+++ Source/Assets/Scripts/Utils/DontDestroyOnLoad.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HART
+{
+
+    public class DontDestroyOnLoad : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+            GameObject.DontDestroyOnLoad(this.gameObject);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Source/Assets/Scripts/Utils/Singular.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singular : SingletonBehavior<Singular> {
+
+
+	private static Singular oneInstance;
+	void Awake(){
+		_CheckSingularity ();
+	}
+
+
+	void _CheckSingularity() {
+		Debug.Log ("SING.AWAKE");
+
+		if (oneInstance == null) {
+			oneInstance = this;
+
+			//			this.gameObject.transform.SetParent (null);
+		
+
+			DontDestroyOnLoad (this.gameObject);
+			//we belong to nobody!.
+			Debug.Log ("SING.DDD");
+		} else {
+			//wipe out any new ones.
+			DestroyObject(this.gameObject);
+			Debug.Log ("SING.TOOMANY");
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
