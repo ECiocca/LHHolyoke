9514c5f98b92a2c0c0aeaf00254f229739e9f585
Joe <joblezlll@gmail.com>
2019-03-26 09:16:02 -0400

    weapon switch added

--- Source/Assets/Prefabs/Level Prefabs/Player/Pistol/Shoot2.cs
+++ Source/Assets/Prefabs/Level Prefabs/Player/Pistol/Shoot2.cs
@@ -1,33 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+ 
 public class Shoot2 : MonoBehaviour
 {
-
+ 
     public GameObject Bullet;
     public GameObject Gunend;
-
+ 
     public float speed = 1.0F;
     public float fireRate = 0.5F;
     private float nextFire = 0.0F;
-
+ 
     float x = 1;
     bool r = false;
-
-    public AudioClip nuke;
+ 
+    public AudioClip loop;
+    public AudioClip loopend;
     public AudioClip Reload;
     public AudioSource m_AudioSource;
-
+ 
     public Gun ThisGun;
-
-
+ 
+ 
     // Use this for initialization
     void Start()
     {
         ThisGun = this.gameObject.GetComponent<Gun>();
     }
-
+ 
     // Update is called once per frame
     void Update()
     {
@@ -35,25 +36,38 @@ public class Shoot2 : MonoBehaviour
         {
             if (ThisGun.CurrentAmmo > 0)
             {
-                
+               
                 GameObject go = GameObject.Instantiate(Bullet);
                 ThisGun.Shoot();
                 go.transform.position = Gunend.transform.position;
                 go.GetComponent<Rigidbody>().AddForce(Vector3.Cross(this.transform.up, this.transform.forward) * speed, ForceMode.Impulse);
                 nextFire = Time.time + fireRate;
            
-                m_AudioSource.clip = nuke;
-                m_AudioSource.Play();
-
+                m_AudioSource.clip = loop;
+                m_AudioSource.loop = false;
+         
+ 
+ 
                 Animator anm = GetComponent<Animator>();
                 if (anm != null)
                 {
                     anm.Play("Shoot");
                     nextFire = Time.time + fireRate;
+                   
                 }
-
+ 
+                m_AudioSource.Play();
+ 
             }
-
+ 
+            else
+            {
+             //   m_AudioSource.Play();
+             //   m_AudioSource.clip = loopend;
+            //    m_AudioSource.loop = false;
+               
+            }
+ 
         }
         else
         {
@@ -61,16 +75,17 @@ public class Shoot2 : MonoBehaviour
             {
                 if (ThisGun.CurrentAmmo < ThisGun.MaxAmmo()) //If your clip isn't full
                 {
-
+ 
                     Animator anm = GetComponent<Animator>();
                     if (anm != null)
                     {
                         anm.Play("Reload");
                     }
-
+ 
                     x = ThisGun.RealoadingSpeed();
                     r = true;
                     m_AudioSource.clip = Reload;
+                    m_AudioSource.loop = false;
                     m_AudioSource.Play();
                 }
             }
@@ -87,7 +102,7 @@ public class Shoot2 : MonoBehaviour
             x = 1;
             r = false;
         }
-
+ 
     }
-
+ 
 }
---
+++ Source/Assets/WeaponSwitcher.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwitcher : MonoBehaviour {
+
+    public int weaponnumber = 0;
+
+    public Gun[] Weapons;
+       
+	// Use this for initialization
+	void Start () {
+        SelectWeapon();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int lastselected = weaponnumber;
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+            
+
+            if (weaponnumber >= Weapons.Length - 1)
+                weaponnumber = 0;
+            else
+                weaponnumber++;
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+            if (weaponnumber <= 0)
+                weaponnumber = Weapons.Length - 1;
+            else
+                weaponnumber--;
+        }
+
+       
+
+        if (Input.GetKeyDown(KeyCode.Alpha2)) 
+        {
+            weaponnumber = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            weaponnumber = 1;
+        }
+
+        if (lastselected != weaponnumber)
+        {
+            SelectWeapon();
+        }
+
+    }
+
+    void SelectWeapon ()
+    {
+        int i = 0;
+        foreach (Gun gun in Weapons )
+        {
+            if (i == weaponnumber)
+                gun.gameObject.SetActive(true);
+
+            else
+                gun.gameObject.SetActive(false);
+
+
+            i++;
+
+
+        }
+    }
+
+}
