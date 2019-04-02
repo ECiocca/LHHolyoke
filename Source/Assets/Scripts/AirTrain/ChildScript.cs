4a672ccad7ef3323abddb9761863aa1001135e47
Max <magicmaxmark@gmail.com>
2019-04-02 10:40:51 -0400

    made it so you can leave the plane

--- Source/Assets/Scripts/AirTrain/ChildScript.cs
+++ Source/Assets/Scripts/AirTrain/ChildScript.cs
@@ -23,7 +23,7 @@ public class ChildScript : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.E) && plane.transform.position.x - this.transform.position.x <= 4 && plane.transform.position.y - this.transform.position.y <= 4 && plane.transform.position.z - this.transform.position.z <= 4 && transform.parent == plane.transform)
         {
-            gameObject.transform.parent = plane.gameObject.transform;
+            gameObject.transform.parent = null;
             gameObject.transform.localPosition = new Vector3(80, 300, 85);
             gameObject.transform.localRotation = Quaternion.Euler(0, 0, 0);
         }
