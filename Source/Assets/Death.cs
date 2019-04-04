fc1a304199fcb996e96571ed3be34a55f2788e2b
Eric <ericciocca@gmail.com>
2019-04-04 10:20:21 -0400

    removed death room

--- Source/Assets/Death.cs
+++ Source/Assets/Death.cs
@@ -12,15 +12,54 @@ public class Death : MonoBehaviour{
     public GameObject Lookat;
     public Shoot Zoom;
 
+    public GameObject deathUI;
+
+
     public float CurrentHealth = 100f;
     public float HCooldown;
 
     private void Start()
     {
+        deathUI.SetActive(false);
         Health myHealth = this.gameObject.GetComponent<Health>();
         CurrentHealth = myHealth.StartHealth;
         
     }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            CurrentHealth = 0;
+            HCooldown = 10;
+        }
+
+        if (CurrentHealth <= 0)
+        {
+            Player.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = false;
+            Player.GetComponent<Rigidbody>().velocity = Vector3.zero;
+            Player.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            //Camera.transform.position = DeathScrene.transform.position;
+            //Camera.transform.LookAt(Lookat.transform);
+            Zoom.Magnifucation = 15;
+            //if (HUD != null)
+            //{
+            //    HUD.gameObject.SetActive(false);
+            //}
+            GameObject.Destroy(Body.gameObject);
+
+            if (deathUI != null)
+            {
+                deathUI.SetActive(true);
+            }
+
+            Cursor.lockState = CursorLockMode.None;
+
+
+        }
+    }
+
+   
 
     private void OnCollisionEnter(Collision other)
     {
@@ -38,23 +77,9 @@ public class Death : MonoBehaviour{
                 HCooldown = 5;
                 
             }
-            if (CurrentHealth <= 0)
-            {
-                Player.GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController>().enabled = false;
-                Player.GetComponent<Rigidbody>().velocity = Vector3.zero;
-                Player.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
-                Camera.transform.position = DeathScrene.transform.position;
-                Camera.transform.LookAt(Lookat.transform);
-                Zoom.Magnifucation = 15;
-                if (HUD != null)
-                {
-                    HUD.gameObject.SetActive(false);
-                }
-                GameObject.Destroy(Body.gameObject);
+        }
 
-
-            }
-        }       
-      }
+       
+    }
     }
 
