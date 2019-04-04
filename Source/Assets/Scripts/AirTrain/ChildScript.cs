f710c1d916dac17d4dcb021fbe70c18db0d1c0d3
Max <magicmaxmark@gmail.com>
2019-04-04 09:45:56 -0400

    merging

--- Source/Assets/Scripts/AirTrain/ChildScript.cs
+++ Source/Assets/Scripts/AirTrain/ChildScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ChildScript : MonoBehaviour
 {
     public GameObject plane;
+    bool isAirplaneChild = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,18 +15,19 @@ public class ChildScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E) && plane.transform.position.x - this.transform.position.x <= 100 && plane.transform.position.y - this.transform.position.y <= 100 && plane.transform.position.z - this.transform.position.z <= 100 && transform.parent != plane.transform)
+        if (Input.GetKeyUp(KeyCode.E) && plane.transform.position.x - this.transform.position.x <= 100 && plane.transform.position.y - this.transform.position.y <= 100 && plane.transform.position.z - this.transform.position.z <= 100 && transform.parent != plane.transform)
         {
-            gameObject.transform.parent = plane.gameObject.transform;
-            gameObject.transform.localPosition = new Vector3(0, 110, 300);
-            gameObject.transform.localRotation = Quaternion.Euler(0, 0, 0);
+            transform.parent = plane.gameObject.transform;
+            transform.localPosition = new Vector3(0, 110, 300);
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+            isAirplaneChild = true;
         }
 
-        if (Input.GetKeyDown(KeyCode.E) && plane.transform.position.x - this.transform.position.x <= 4 && plane.transform.position.y - this.transform.position.y <= 4 && plane.transform.position.z - this.transform.position.z <= 4 && transform.parent == plane.transform)
+        if (Input.GetKeyUp(KeyCode.E) && isAirplaneChild == true)
         {
-            gameObject.transform.parent = null;
-            gameObject.transform.localPosition = new Vector3(80, 300, 85);
-            gameObject.transform.localRotation = Quaternion.Euler(0, 0, 0);
+            transform.localPosition = new Vector3(80, 300, 85);
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+            isAirplaneChild = false;
         }
     }
 }
